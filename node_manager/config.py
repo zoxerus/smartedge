abcd87ce9bdf0e670cec263524e37baa170338a8
Faris <faris@null.net>
2024-07-23 14:32:17 +0200

    072324

--- node_manager/config.py
+++ node_manager/config.py
@@ -1,6 +1,6 @@
 import psutil 
 loopback_if = 'lo:0'
-loopback_id = psutil.net_if_addrs()[loopback_if][0][1]
+loopback_id = psutil.net_if_addrs()[loopback_if][0][1].split('.')
 
 NODE_UUID = f'SN:{loopback_id[2]:03}'
 
