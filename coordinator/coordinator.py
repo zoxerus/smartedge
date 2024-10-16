0d08495b65d3cb14939e54bf979eccbb2728df7c
Faris <faris@null.net>
2024-10-16 18:34:04 +0200

    101624

--- coordinator/coordinator.py
+++ coordinator/coordinator.py
@@ -112,12 +112,12 @@ class Swarm_Node_Handler:
                                                     thrift_ip= ap_ip, thrift_port= DEFAULT_THRIFT_PORT)
         
         bmv2_thrift.delete_forwarding_entry_from_bmv2(
-            communication_protocol= bmv2_thrift.P4_CONTROL_METHOD_THRIFT_CLI, table_name='MyIngress.tb_swarm_control', key= f'{node_swarm_id} {global_config.coordinator_vip}',
+            communication_protocol= bmv2_thrift.P4_CONTROL_METHOD_THRIFT_CLI, table_name='MyIngress.tb_swarm_control', key= f'{node_swarm_id} {node_swarm_ip}',
             thrift_ip= ap_ip, thrift_port= DEFAULT_THRIFT_PORT)
 
         bmv2_thrift.delete_forwarding_entry_from_bmv2(
             communication_protocol= bmv2_thrift.P4_CONTROL_METHOD_THRIFT_CLI, table_name= 'MyIngress.tb_swarm_control', 
-            key= f'{global_config.swarm_backbone_switch_port} {node_swarm_ip }', thrift_ip= ap_ip, thrift_port=DEFAULT_THRIFT_PORT)
+            key= f'{global_config.swarm_backbone_switch_port} {global_config.coordinator_vip}', thrift_ip= ap_ip, thrift_port=DEFAULT_THRIFT_PORT)
         
         
         # insert table entries in the rest of the APs
--- lib/global_config.py
+++ lib/global_config.py
@@ -8,14 +8,30 @@ swarm_backbone_switch_port = 501
 REDIS_PORT = 6379
 CASSANDRA_PORT = 9402
 
+'''
+In the new documentation, I have replaced the name vxlan1000 with the name smartedge-bb, but the functionality remains the same.
+In this file I am using two networks:
+the 192.168.10.0/24 is the swarm subnet
+the 192.168.100.0/24 is the smartedge backbone and it is a vxlan overlay network used to connect the coordinator with the APs
+The IP of the coordinator is on the backbone network is 192.168.100.6 and on the swarm subnet it takes the IP 192.168.10.1
+and the access points have the IPs 192.168.100.3, 192.168.100.4, and 192.168.10.5.
+currently the Access Points don't take swarm IP (e.g on the subnet 192.168.10.0/24) as it is not needed. 
+'''
+
+
 # here configure the subnet range that need to be allocated to the swarm
 # here we assume a /24 subnet mask
-this_swarm_subnet='192.168.10.0'
-this_swarm_dhcp_start = 2
-this_swarm_dhcp_end  = 200
+this_swarm_subnet='192.168.10.0'  ## this is the subnet to use for the swarm
+this_swarm_dhcp_start = 2         # this is the first IP to be assigned to the swarm node e.g: 192.168.10.2
+this_swarm_dhcp_end  = 200        # last IP to be assigned e.g: 192.168.10.200  so it supports 199 nodes (arbitrary number can be changed to any)
+
 
 # This is the ip and port number of the database
-database_hostname = '172.18.0.2'
+## This is the IP of the database, in my implementation the DB is running on the same node as the coordinator
+## so for the coordinator, this is an internal IP on the docker network.
+## in case of Access Points (and the DB is running on the coordinator node) this would be the IP of the coordinator (IP of the smartedge-bb interface of the coordinator)
+## e.g 192.168.100.6
+database_hostname = '172.18.0.2'  
 database_port = 9042
 
 # This IP is used by the access points to reach the coordinator,
@@ -26,8 +42,8 @@ coordinator_physical_ip = '192.168.100.6'
 # this IP is also configured on the smartedge-bb interface of the coordinator
 coordinator_vip='192.168.10.1'
 
-# this is the mac that is configured on the  smartedge-bb of the coordinator
-coordinator_mac = '3e:a9:ef:fd:15:9e'
+# this is the mac that is of the  smartedge-bb of the coordinator
+coordinator_mac = '3e:a9:ef:fd:15:9e'  
 
 # this is a tcp port number used to reach the coordinator from the swarm nodes
 coordinator_tcp_port = 29997
@@ -42,6 +58,7 @@ default_thrift_port = 9090
 # list of access points in the network, used to propagate configuration changes
 # this list of IPs and MACs are the ones configured on the smartedge-bb on each access point
 ap_list = {
-    'AP:004': ['ae:41:1c:2d:6d:5c', '192.168.100.4'],
-    'AP:003': ['72:33:e0:66:b3:8e', '192.168.100.3']
+    # ID of AP       MAC of smartedge-bb    IP of smartedge-bb
+    'AP:004': [     'ae:41:1c:2d:6d:5c',    '192.168.100.4'],
+    'AP:003': [     '72:33:e0:66:b3:8e',    '192.168.100.3']
     }
