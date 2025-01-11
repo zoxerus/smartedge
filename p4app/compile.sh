f124fd22e3b3fa9199bcf5da15971a54abe68fca
zoxerus <faris@null.net>
2025-01-12 00:23:00 +0100

    Refactoring v1

--- p4app/compile.sh
+++ p4app/compile.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+SCRIPT_PATH="${BASH_SOURCE[0]:-$0}";
+cd "$( dirname -- "$SCRIPT_PATH"; )";
+
+
 p4c --target bmv2 --arch v1model --std p4-16 --output ./ ./ap.p4
 
 # p4c --target bmv2 --arch v1model --std p4-16 --output ./bin ./p4src/ap.p4
--- run_bmv2_docker.sh
+++ run_bmv2_docker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+SCRIPT_PATH="${BASH_SOURCE[0]:-$0}";
+cd "$( dirname -- "$SCRIPT_PATH"; )";
+
+
 if [ "$(sudo docker container inspect -f '{{.State.Status}}' 'bmv2smartedge' )" = "running" ]; then
     echo "Stopping container bmv2smartedge"
     sudo docker stop bmv2smartedge
@@ -10,5 +14,6 @@ sudo docker run --privileged --rm --net=host --volume $(pwd):$(pwd) --workdir $(
 sudo docker exec -d bmv2smartedge  pip install thrift
 if [[ $1 != 'co' ]]; then
     echo -e "AP bmv2"
+    sudo docker exec -d bmv2smartedge /bin/bash ./p4app/compile.sh
     sudo docker exec -d bmv2smartedge  simple_switch ./p4app/ap.json
 fi
