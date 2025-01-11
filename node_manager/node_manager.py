87c253e27cb9135e34d247dc855e78b3d9068e32
zoxerus <faris@null.net>
2025-01-12 00:53:48 +0100

    Refactoring v1

--- node_manager/node_manager.py
+++ node_manager/node_manager.py
@@ -132,7 +132,7 @@ def handle_tcp_communication():
                     install_swarmNode_config()
                 except Exception as e:
                     print(f'Error installing config: {e} Leaving Access Point' )
-                    # continue
+                    continue
                     cli_command = f'nmcli connection show --active'
                     res = subprocess.run(cli_command.split(), text=True, stdout=subprocess.PIPE)
                     ap_ssid = ''
@@ -203,6 +203,7 @@ def exit_handler():
         
         
 def handle_disconnection():
+    return
     logger.debug( '\nHandling Disconnection:\n'  )
     exit_commands = [
         'ifconfig veth1 0.0.0.0',
