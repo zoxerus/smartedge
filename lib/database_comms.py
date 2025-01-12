c36434f8b85a836f5fda55c66b3f9ddd7539ff10
zoxerus <faris@null.net>
2025-01-12 01:44:16 +0100

    Refactoring v1

--- lib/database_comms.py
+++ lib/database_comms.py
@@ -103,9 +103,8 @@ def get_next_available_host_id_from_swarm_table(first_host_id, max_host_id):
             {db_defines.NAMEOF_DATABASE_SWARM_KEYSPACE}.{db_defines.NAMEOF_DATABASE_SWARM_TABLE_ACTIVE_NODES}"""
         result = DATABASE_SESSION.execute(query)
         print('got results: ', result)
-        db_logger.debug(f"Executed database query:\n\t {query}\n\tgot result:\n\t\t{result.all()}")
+        db_logger.debug(f"Executed database query:\n\t {query}\n\tgot result:\n\t\t{result}")
         id_list = []
-
         for row in result:
             db_logger.debug(f"received Row from DB: {row}")
             id_list.append(row[0])
