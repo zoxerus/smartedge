6558bfd287b42a4c1ec766757482a308c47c99ae
zoxerus <faris@null.net>
2025-01-14 14:06:28 +0100

    ref

--- run_bmv2_docker.sh
+++ run_bmv2_docker.sh
@@ -6,9 +6,10 @@ if [ "$(sudo docker container inspect -f '{{.State.Status}}' 'bmv2smartedge' )" 
 fi
 
 echo "Starting Container bmv2smartedge"
-sudo docker run --privileged --rm --net=host --volume $(pwd):$(pwd) --workdir $(pwd) -i -t --name bmv2smartedge -d p4jet
+sudo docker run --privileged --rm --net=host --volume $(pwd):$(pwd) --workdir $(pwd) -i -t --name bmv2smartedge -d bmv2se
 sudo docker exec -d bmv2smartedge  pip install thrift
 if [[ $1 != 'co' ]]; then
     echo -e "AP bmv2"
+    sudo docker exec -d bmv2smartedge ./p4app/compile.sh
     sudo docker exec -d bmv2smartedge  simple_switch ./p4app/ap.json
 fi
