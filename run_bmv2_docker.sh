0a63310094a75197e4d5fd9df4a853df4f54bdc4
zoxerus <faris@null.net>
2025-01-15 10:36:17 +0100

    fix mac issue with APs

--- run_bmv2_docker.sh
+++ run_bmv2_docker.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if [ "$(sudo docker container inspect -f '{{.State.Status}}' 'bmv2smartedge' )" = "running" ]; then
-    echo "Stopping container bmv2smartedge"
-    sudo docker stop bmv2smartedge
-    sleep 1
-fi
+# if [ "$(sudo docker container inspect -f '{{.State.Status}}' 'bmv2smartedge' )" = "running" ]; then
+#     echo "Stopping container bmv2smartedge"
+#     sudo docker stop bmv2smartedge
+#     sleep 1
+# fi
 
 echo "Starting Container bmv2smartedge"
 sudo docker run --privileged --rm --net=host --volume $(pwd):$(pwd) --workdir $(pwd) -i -t --name bmv2smartedge -d bmv2se
 sudo docker exec -d bmv2smartedge  pip install thrift
 if [[ $1 != 'co' ]]; then
     echo -e "AP bmv2"
-    
+
     sudo docker exec -d bmv2smartedge ./p4app/compile.sh
     sudo docker exec -d bmv2smartedge  simple_switch ./p4app/ap.json
 fi
