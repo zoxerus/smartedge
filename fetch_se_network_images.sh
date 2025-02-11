39c49fa007a8092b26e84117265b7873292d1484
zoxerus <faris@null.net>
2025-02-11 17:05:36 +0100

    preparing demo

--- fetch_se_network_images.sh
+++ fetch_se_network_images.sh
@@ -1,1 +1,12 @@
 wget -O se_network.tar https://tinyurl.com/54ntajmw
+
+expected_md5="4b0180a9b94289d097610ce43655d9e8"
+calculated_md5=$(md5sum myfile.txt | awk '{print $1}')
+
+if [ "$calculated_md5" = "$expected_md5" ]; then
+    echo "All good, MD5 hash matches"
+else
+    echo "Error: Attention! MD5 hash does NOT match"
+fi
+
+
