b159951cea5ffa3abf1191ebeb58b847d8e4aa73
zoxerus <faris@null.net>
2025-04-12 17:00:01 +0200

    bug

--- lib/bmv2_thrift_lib.py
+++ lib/bmv2_thrift_lib.py
@@ -78,6 +78,7 @@ def remove_bmv2_swarm_broadcast_port(ap_ip, thrift_port, switch_port ):
         for line in res_lines:
             if 'mgrp(' in line:
                 port_list = set(extract_numbers([ res_lines[i+1].split('ports=[')[1].split(']')[0] ]))
+                bmv2_logger.debug("Port_list = " + str(port_list) )
                 port_list.remove(switch_port)
                 broadcast_ports =  ' '.join( str(port) for port in port_list)
                 send_cli_command_to_bmv2(f"mc_node_update 0 {broadcast_ports} ", ap_ip, thrift_port )  
