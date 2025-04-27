8ab2e740613940f10b8507ba5d36756b52998465
zoxerus <faris@null.net>
2025-04-27 19:19:29 +0200

    bug fix

--- lib/bmv2_thrift_lib.py
+++ lib/bmv2_thrift_lib.py
@@ -77,7 +77,7 @@ def run_cli_command(command, instance):
 
 
 
-def send_cli_command_to_bmv2(cli_command, instance, thrif_ip='0.0.0.0', thrift_port=DEFAULT_THRIFT_PORT ):
+def send_cli_command_to_bmv2(cli_command, instance, thrift_ip='0.0.0.0', thrift_port=DEFAULT_THRIFT_PORT ):
     return run_cli_command(command=cli_command, instance=instance)
     command_as_word_array = ['docker','exec',BMV2_DOCKER_CONTAINER_NAME,'sh', '-c',
                              f"echo \'{cli_command}\' | simple_switch_CLI --thrift-ip {args.thrift_ip} --thrift-port {args.thrift_port}"  ]
